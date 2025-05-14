5650e24a446d916e2897a6ec2e41fc96141260b9
fangyu <fangyuliu212@gmail.com>
2025-05-14 10:45:01 +0200

    Add files via upload

---
+++ eOR1-IA-FLX-11/create_link.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+datadir=/ccc/store/cont003/gen1140/liufangy
+JobName=eOR1L3P-IA-BLK-03-fy
+ExperimentName=ORCA1ia
+SpaceName=DEVT
+TagName=ORCA1_LIM3_PISCES
+dirout=${datadir}/IGCM_OUT/${TagName}/${SpaceName}/${ExperimentName}/${JobName}
+export YearBegin=1980
+export YearEnd=2018
+(( YearFirst = ${YearBegin} - 1 ))
+(( YearLast  = ${YearEnd}   + 1 ))
+
+cd ${dirout}/FORCINGS_TEST_Srinivas
+
+yr=${YearBegin}
+while [ ${yr} -le ${YearEnd} ] ; do
+  #clm
+  ln -sf ${dirout}/FORCINGS/clm_1980-2018/${JobName}_clim1980-2018_3H_SBC.nc                          hflux_${yr}.nc
+
+  #ano
+  ln -sf ${dirout}/FORCINGS/ano_subtract_ANL/${JobName}_${yr}0101_${yr}1231_ano-ANL_1M_grid_U.nc      utau_ano_${yr}.nc
+  ln -sf ${dirout}/FORCINGS/ano_subtract_ANL/${JobName}_${yr}0101_${yr}1231_ano-ANL_1M_grid_V.nc      vtau_ano_${yr}.nc
+  ln -sf ${dirout}/FORCINGS/clm_1980-2018/${JobName}_clim1980-2018_3H_SBC.nc                          hflux_ano_${yr}.nc
+  
+  #total
+  ln -sf ${dirout}/OCE/Output/HF/${JobName}_${yr}0101_${yr}1231_3H_SBC.nc                             fflux_${yr}.nc
+  ln -sf ${dirout}/ICE/Output/HF/${JobName}_${yr}0101_${yr}1231_3H_icemod.nc                          ice_${yr}.nc
+
+  (( yr = ${yr} + 1 ))
+done
+
+ln -sf ${dirout}/FORCINGS/clm_1980-2018/${JobName}_clim1980-2018_3H_SBC.nc                                         hflux_${YearFirst}.nc
+ln -sf ${dirout}/FORCINGS/ano_subtract_ANL/${JobName}_${YearBegin}0101_${YearBegin}1231_ano-ANL_1M_grid_U.nc       utau_ano_${YearFirst}.nc
+ln -sf ${dirout}/FORCINGS/ano_subtract_ANL/${JobName}_${YearBegin}0101_${YearBegin}1231_ano-ANL_1M_grid_V.nc       vtau_ano_${YearFirst}.nc
+ln -sf ${dirout}/FORCINGS/clm_1980-2018/${JobName}_clim1980-2018_3H_SBC.nc                                         hflux_ano_${YearFirst}.nc
+ln -sf ${dirout}/OCE/Output/HF/${JobName}_${YearBegin}0101_${YearBegin}1231_3H_SBC.nc                              fflux_${YearFirst}.nc
+ln -sf ${dirout}/ICE/Output/HF/${JobName}_${YearBegin}0101_${YearBegin}1231_3H_icemod.nc                           ice_${YearFirst}.nc
+
+ln -sf ${dirout}/FORCINGS/clm_1980-2018/${JobName}_clim1980-2018_3H_SBC.nc                                         hflux_${YearLast}.nc
+ln -sf ${dirout}/FORCINGS/ano_subtract_ANL/${JobName}_${YearEnd}0101_${YearEnd}1231_ano-ANL_1M_grid_U.nc           utau_ano_${YearLast}.nc
+ln -sf ${dirout}/FORCINGS/ano_subtract_ANL/${JobName}_${YearEnd}0101_${YearEnd}1231_ano-ANL_1M_grid_V.nc           vtau_ano_${YearLast}.nc
+ln -sf ${dirout}/FORCINGS/clm_1980-2018/${JobName}_clim1980-2018_3H_SBC.nc                                         hflux_ano_${YearLast}.nc
+ln -sf ${dirout}/OCE/Output/HF/${JobName}_${YearEnd}0101_${YearEnd}1231_3H_SBC.nc                                  fflux_${YearLast}.nc
+ln -sf ${dirout}/ICE/Output/HF/${JobName}_${YearEnd}0101_${YearEnd}1231_3H_icemod.nc                               ice_${YearLast}.nc
+
